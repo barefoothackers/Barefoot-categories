21acc2ef5180fd5e8c9ad52c6c4e43d31f61e829
Leo Giertz <leo@barefoothackers.se>
2013-07-26 15:58:48 +0200

    It's now ARC only.

--- NSDate+Barefoot.m
+++ NSDate+Barefoot.m
@@ -3,13 +3,13 @@
 @implementation NSDate (Barefoot)
 
 - (NSString *)stringWithFormat:(NSString *)format {
-  NSDateFormatter *dateFormatter = [[[NSDateFormatter alloc] init] autorelease];
+  NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
   [dateFormatter setDateFormat:format];
   return [dateFormatter stringFromDate:self];
 }
 
 - (NSString *)stringWithDateStyle:(NSDateFormatterStyle)dateStyle timeStyle:(NSDateFormatterStyle)timeStyle {
-  NSDateFormatter *dateFormatter = [[[NSDateFormatter alloc] init] autorelease];
+  NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
   [dateFormatter setDateStyle:dateStyle];
   [dateFormatter setTimeStyle:timeStyle];
   return [dateFormatter stringFromDate:self];
--- UITableView+Barefoot.m
+++ UITableView+Barefoot.m
@@ -5,7 +5,7 @@
 - (UITableViewCell *)dequeueOrAllocInitAutoreleaseReusableCellWithIdentifier:(NSString *)identifier {
   UITableViewCell *cell = [self dequeueReusableCellWithIdentifier:identifier];
   if (!cell)
-    cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier] autorelease];
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
   return cell;
 }
 
